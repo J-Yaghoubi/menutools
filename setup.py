ad4f8cfecce6428e31acb578cdd07ade53e6f28f
s.jafar.yaghoubi <algo3xp3rt@gmail.com>
2022-10-22 18:34:18 +0330

    Release 1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='menutools',
-    version='1.0.4',
+    version='1.0.5',
     description='Create command line menu',
     long_description=long_description,
     long_description_content_type='text/markdown',
